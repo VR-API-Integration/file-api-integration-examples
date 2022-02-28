e6fee920dcef88dec05d7598f912ce5e0d1127bb
AlbertoInf <alberto.infante@visma.com>
2022-02-28 08:50:49 +0100

    vrftr-3461 vrftr-4258: [PowerShell] Add option to ensure unique names on downloads

--- powershell-with-no-dependencies/DownloadFiles.ps1
+++ powershell-with-no-dependencies/DownloadFiles.ps1
@@ -4,9 +4,14 @@ Write-Host "================================================"
 Write-Host "File API example: Download available files."
 Write-Host "================================================"
 
+Write-Host "(you can stop the script at any moment by pressing the buttons 'CTRL'+'C')"
+
 #region Configuration
 
-$configPath = "$($PSScriptRoot)\config.xml"
+# XXX
+# $configPath = "$($PSScriptRoot)\config.xml"
+$configPath = "C:\Users\AlbertoInf\Inbox\PBI FTaaS Improve curl examples\config.xml"
+# XXX Ensure you can actually use the [xml] thing.
 [xml]$configDocument = Get-Content $configPath
 $config = $configDocument.Configuration
 
@@ -17,6 +22,7 @@ $listFilter = $config.List.Filter
 
 $role = $Config.Download.Role
 $downloadPath = $config.Download.Path
+$ensureUniqueNames = $config.Download.EnsureUniqueNames
 
 $authTokenApiBaseUrl = "https://api.raet.com/authentication"
 $fileApiBaseUrl = "https://api.raet.com/mft/v1.0"
@@ -27,18 +33,21 @@ $fileApiBaseUrl = "https://api.raet.com/mft/v1.0"
 
 Write-Host "Retrieving the authentication token..."
 
-$authHeaders = @{
-    "Content-Type"  = "application/x-www-form-urlencoded";
-    "Cache-Control" = "no-cache";
-}
-$authBody = @{
-    "grant_type"    = "client_credentials";
-    "client_id"     = $clientId;
-    "client_secret" = $clientSecret;
-}
+# $authHeaders = @{
+#     "Content-Type"  = "application/x-www-form-urlencoded";
+#     "Cache-Control" = "no-cache";
+# }
+# $authBody = @{
+#     "grant_type"    = "client_credentials";
+#     "client_id"     = $clientId;
+#     "client_secret" = $clientSecret;
+# }
 
-$authTokenResponse = Invoke-RestMethod -Method "Post" -Uri "$($authTokenApiBaseUrl)/token" -Headers $authHeaders -Body $authBody
-$token = $authTokenResponse.access_token
+# $authTokenResponse = Invoke-RestMethod -Method "Post" -Uri "$($authTokenApiBaseUrl)/token" -Headers $authHeaders -Body $authBody
+# $token = $authTokenResponse.access_token
+
+# XXX
+$token = $config.XXXToken
 
 Write-Host "Authentication token retrieved."
 
@@ -48,23 +57,35 @@ Write-Host "Authentication token retrieved."
 
 Write-Host "Calling the 'list' endpoint..."
 
-$listHeaders = @{
-    "x-raet-tenant-id" = $tenantId;
-    "Authorization"    = "Bearer $($token)";
-}
-$listBody = @{
-    '$filter' = $listFilter;
-    "role"    = $role
-}
+# $listHeaders = @{
+#     "x-raet-tenant-id" = $tenantId;
+#     "Authorization"    = "Bearer $($token)";
+# }
+# $listBody = @{
+#     '$filter' = $listFilter;
+#     "role"    = $role
+# }
 
-$listResponse = Invoke-RestMethod -Method "Get" -Uri "$($fileApiBaseUrl)/files" -Headers $listHeaders -Body $listBody
-$filesToDownload = @()
-foreach ($fileData in $listResponse.data) {
-    $filesToDownload += @{
-        Id   = $fileData.fileId
-        Name = $fileData.fileName
+# $listResponse = Invoke-RestMethod -Method "Get" -Uri "$($fileApiBaseUrl)/files" -Headers $listHeaders -Body $listBody
+# # $randomBinFiles = $content.links | where {$_.innerHTML -like 'random*'} | select href
+# $filesToDownload = @()
+# foreach ($fileData in $listResponse.data) {
+#     $filesToDownload += @{
+#         Id   = $fileData.fileId
+#         Name = $fileData.fileName
+#     }
+# }
+
+$filesToDownload = @(
+    @{
+        Id   = "c5d7438e-5c29-47e7-b518-01e5a39d6711"
+        Name = "sandbox_test_file.txt"
+    },
+    @{
+        Id   = "d92ffb21-7938-488b-a405-bcbc9e0a9696"
+        Name = "sandbox_test_file.xml"
     }
-}
+)
 
 Write-Host "List of files retrieved."
 
@@ -84,7 +105,23 @@ $downloadBody = @{
 }
 
 foreach ($fileToDownload in $filesToDownload) {
-    Invoke-RestMethod -Method "Get" -Uri "$($fileApiBaseUrl)/files/$($fileToDownload.Id)" -Headers $downloadHeaders -Body $downloadBody -OutFile "$($downloadPath)\$($fileToDownload.Name)"
+    Write-Host "Downloading file <$($fileToDownload.Id)> with name <$($fileToDownload.Name)>."
+    if (($ensureUniqueNames -eq $true) -and (Test-Path "$($downloadPath)\$($fileToDownload.Name)" -PathType Leaf)) {
+        Write-Host "There is already a file with the same name in the specified path."
+        Write-Host "Renaming the file to be downloaded."
+        $fileNameWithoutExtension = Split-Path $fileToDownload.Name -LeafBase # XXX This shit doesn't work, create your own method.
+        $fileExtension = Split-Path $fileToDownload.Name -Extension
+        $timestamp = Get-Date -Format FileDateTimeUniversal
+        $fileToDownload.Name = "$($fileNameWithoutExtension) - $($timestamp)$($fileExtension)"
+        Write-Host "File has been renamed to <$($fileToDownload.Name)>."
+    }
+
+    Invoke-RestMethod `
+        -Method "Get" `
+        -Uri "$($fileApiBaseUrl)/files/$($fileToDownload.Id)" `
+        -Headers $downloadHeaders  `
+        -Body $downloadBody -OutFile "$($downloadPath)\$($fileToDownload.Name)"
+        
     Write-Host "File <$($fileToDownload.Id)> with name <$($fileToDownload.Name)> was downloaded."
 }
 
