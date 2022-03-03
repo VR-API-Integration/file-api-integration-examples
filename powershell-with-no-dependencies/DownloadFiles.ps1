16aaccf6c7610df4254fece1ca5957528d5ca8d4
AlbertoInf <alberto.infante@visma.com>
2022-03-03 13:03:37 +0100

    vrftr-3461 vrftr-4258: [PowerShell] Improve readability

--- powershell-with-no-dependencies/DownloadFiles.ps1
+++ powershell-with-no-dependencies/DownloadFiles.ps1
@@ -6,44 +6,113 @@ Write-Host "========================================================="
 
 Write-Host "(you can stop the script at any moment by pressing the buttons 'CTRL'+'C')"
 
-#region Helpers
+#region Configuration
 
-function Get-FileNameInfo_FileAPIHelper {
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory = $true)]
-        [string] $fileName
-    )
+# XXX
+# $configPath = "$($PSScriptRoot)\config.xml"
+$configPath = "C:\Users\AlbertoInf\Inbox\PBI FTaaS Improve curl examples\config.xml"
+$configDocument = [xml](Get-Content $configPath)
+$config = $configDocument.Configuration
 
-    $fileNameInfo = @{
-        Name      = $fileName
-        Extension = ""
+$clientId = $config.Credentials.ClientId
+$clientSecret = $config.Credentials.ClientSecret
+$tenantId = $config.TenantId
+$filter = $config.List.Filter
+
+$role = $Config.Download.Role
+$downloadPath = $config.Download.Path
+$ensureUniqueNames = $config.Download.EnsureUniqueNames
+
+# $authTokenApiBaseUrl = "https://api.raet.com/authentication"
+# $fileApiBaseUrl = "https://api.raet.com/mft/v1.0"
+$authTokenApiBaseUrl = "https://api-test.raet.com/authentication"
+$fileApiBaseUrl = "https://api-test.raet.com/mft/v1.0"
+
+#endregion Configuration
+
+#region Retrieve_authentication_token
+
+Write-Host "----"
+Write-Host "Retrieving the authentication token..."
+
+# [AuthenticationApiService] $authenticationApiService = [AuthenticationApiService]::new($authTokenApiBaseUrl)
+# $token = $authenticationApiService.NewToken($clientId, $clientSecret)
+
+# XXX
+$token = $config.XXXToken
+
+Write-Host "Authentication token retrieved."
+
+#endregion Retrieve_authentication_token
+
+#region List
+
+Write-Host "----"
+Write-Host "Retrieving the files that fulfill the filter <$($filter)>..."
+
+[FileApiService] $fileApiService = [FileApiService]::new(
+    $fileApiBaseUrl,
+    $tenantId,
+    $token
+)
+
+$pageIndex = 0
+$pageSize = 20
+$isLastPage = $false
+$filesInfo = @()
+do {
+    $listResponse = $fileApiService.ListFiles($role, $pageIndex, $pageSize, $filter)
+
+    foreach ($fileData in $listResponse.data) {
+        $filesInfo += @{
+            Id   = $fileData.fileId
+            Name = $fileData.fileName
+            Size = $fileData.fileSize
+        }
     }
-    
-    $splitFileName = $fileName -split "\."
-    if ($splitFileName.Length -gt 1) {
-        $fileNameInfo.Name = $splitFileName[0..($splitFileName.Length - 2)] -Join "."
-        $fileNameInfo.Extension = ".$($splitFileName[-1])"
+
+    $isLastPage = $pageSize * ($pageIndex + 1) -ge $listResponse.count
+    $pageIndex++
+} while (-not $isLastPage)
+
+Write-Host "$($filesInfo.Count) files retrieved."
+
+#endregion List
+
+#region Download
+
+$downloadedFilesCount = 0
+foreach ($fileInfo in $filesInfo) {
+    Write-Host "----"
+    Write-Host "Downloading file $($downloadedFilesCount + 1)/$($filesInfo.Count)."
+    Write-Host "| ID: $($fileInfo.Id)"
+    Write-Host "| Name: $($fileInfo.Name)"
+
+    if (($ensureUniqueNames -eq $true) -and (Test-Path "$($downloadPath)\$($fileInfo.Name)" -PathType Leaf)) {
+        Write-Host "There is already a file with the same name in the download path."
+
+        # $fileInfo.Name = ConvertTo-UniqueName_FileAPIHelper $fileInfo.Name
+        $fileInfo.Name = [FileNameHelper]::ConverToUnique($fileInfo.Name) # XXX Not working.
+
+        Write-Host "| New name: $($fileInfo.Name)"
     }
 
-    return $fileNameInfo
+    $fileApiService.DownloadFile($role, $fileInfo, $downloadPath)
+    $downloadedFilesCount++
+        
+    Write-Host "The file was downloaded."
 }
 
-function ConvertTo-UniqueName_FileAPIHelper {
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory = $true)]
-        [string] $fileName
-    )
+Write-Host "----"
+Write-Host "All files were downloaded."
+Write-Host "| Path: $($downloadPath)"
 
-    $fileNameInfo = Get-FileNameInfo_FileAPIHelper $fileName
-    $fileNameWithoutExtension = $fileNameInfo.Name
-    $fileExtension = $fileNameInfo.Extension
-    $timestamp = Get-Date -Format FileDateTimeUniversal
+#endregion Download
 
-    $uniqueFileName = "$($fileNameWithoutExtension) - $($timestamp)$($fileExtension)"
-    return $uniqueFileName
-}
+# -------- END OF THE PROGRAM --------
+# Bellow there are classes to help the readability of the program
+
+#region Classes
 
 class FileApiService {
     [string] $BaseUrl
@@ -115,110 +184,31 @@ class AuthenticationApiService {
     }
 }
 
-#endregion
+class FileNameHelper {
+    static [string] ConverToUnique([string] $fileName) {
+        $fileNameInfo = [FileNameHelper]::GetInfo($fileName)
+        $fileNameWithoutExtension = $fileNameInfo.Name
+        $fileExtension = $fileNameInfo.Extension
+        $timestamp = Get-Date -Format FileDateTimeUniversal
+    
+        $uniqueFileName = "$($fileNameWithoutExtension) - $($timestamp)$($fileExtension)"
+        return $uniqueFileName
+    }
 
-#region Program
-
-#region Configuration
-
-# XXX
-# $configPath = "$($PSScriptRoot)\config.xml"
-$configPath = "C:\Users\AlbertoInf\Inbox\PBI FTaaS Improve curl examples\config.xml"
-[xml]$configDocument = Get-Content $configPath
-$config = $configDocument.Configuration
-
-$clientId = $config.Credentials.ClientId
-$clientSecret = $config.Credentials.ClientSecret
-$tenantId = $config.TenantId
-$filter = $config.List.Filter
-
-$role = $Config.Download.Role
-$downloadPath = $config.Download.Path
-$ensureUniqueNames = $config.Download.EnsureUniqueNames
-
-# $authTokenApiBaseUrl = "https://api.raet.com/authentication"
-# $fileApiBaseUrl = "https://api.raet.com/mft/v1.0"
-$authTokenApiBaseUrl = "https://api-test.raet.com/authentication"
-$fileApiBaseUrl = "https://api-test.raet.com/mft/v1.0"
-
-#endregion
-
-#region Retrieve_authentication_token
-
-Write-Host "----"
-Write-Host "Retrieving the authentication token..."
-
-# [AuthenticationApiService] $authenticationApiService = [AuthenticationApiService]::new($authTokenApiBaseUrl)
-# $token = $authenticationApiService.NewToken($clientId, $clientSecret)
-
-# XXX
-$token = $config.XXXToken
-
-Write-Host "Authentication token retrieved."
-
-#endregion
-
-#region List
-
-Write-Host "----"
-Write-Host "Retrieving the files that fulfill the filter <$($filter)>..."
-
-[FileApiService] $fileApiService = [FileApiService]::new(
-    $fileApiBaseUrl,
-    $tenantId,
-    $token
-)
-
-$pageIndex = 0
-$pageSize = 20
-$isLastPage = $false
-$filesInfo = @()
-do {
-    $listResponse = $fileApiService.ListFiles($role, $pageIndex, $pageSize, $filter)
-
-    foreach ($fileData in $listResponse.data) {
-        $filesInfo += @{
-            Id   = $fileData.fileId
-            Name = $fileData.fileName
-            Size = $fileData.fileSize
+    static [PSCustomObject] GetInfo([string] $fileName) {
+        $fileNameInfo = @{
+            Name      = $fileName
+            Extension = ""
         }
-    }
-
-    $isLastPage = $pageSize * ($pageIndex + 1) -ge $listResponse.count
-    $pageIndex++
-} while (-not $isLastPage)
-
-Write-Host "$($filesInfo.Count) files retrieved."
-
-#endregion
-
-#region Download
-
-$downloadedFilesCount = 0
-foreach ($fileInfo in $filesInfo) {
-    Write-Host "----"
-    Write-Host "Downloading file $($downloadedFilesCount + 1)/$($filesInfo.Count)."
-    Write-Host "| ID: $($fileInfo.Id)"
-    Write-Host "| Name: $($fileInfo.Name)"
-
-    if (($ensureUniqueNames -eq $true) -and (Test-Path "$($downloadPath)\$($fileInfo.Name)" -PathType Leaf)) {
-        Write-Host "There is already a file with the same name in the download path."
-
-        $fileInfo.Name = ConvertTo-UniqueName_FileAPIHelper $fileInfo.Name
-
-        Write-Host "| New name: $($fileInfo.Name)"
-    }
-
-    $fileApiService.DownloadFile($role, $fileInfo, $downloadPath)
-    $downloadedFilesCount++
         
-    Write-Host "The file was downloaded."
+        $splitFileName = $fileName -split "\."
+        if ($splitFileName.Length -gt 1) {
+            $fileNameInfo.Name = $splitFileName[0..($splitFileName.Length - 2)] -Join "."
+            $fileNameInfo.Extension = ".$($splitFileName[-1])"
+        }
+    
+        return $fileNameInfo
+    }
 }
 
-Write-Host "----"
-Write-Host "All files were downloaded."
-Write-Host "| Path: $($downloadPath)"
-
-#endregion
-
-#endregion
+#endregion Classes
