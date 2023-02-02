ff747686cde823e7010ca883a79139b90b8a651d
Mediavilla, Sandra <SandraMed@youforce.net>
2023-02-02 08:42:21 +0100

    vrftr-3340 vrftr-5848 Add delete examples

---
+++ net-core-with-file-api-sdk/FileAPI.MFT.Streaming.NetCore22/Examples/Delete.cs
@@ -0,0 +1,43 @@
+﻿using FileAPI.MFT.Utils;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace FileAPI.MFT.Streaming.NetCore22.Examples
+{
+    public class Delete : Startup
+    {
+        private readonly ITestOutputHelper _output;
+
+        public Delete(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public async Task SetSubscriberFileToDeleted()
+        {
+            // To set a file to 'deleted', you need to provide its file Id.
+            // Also, if you have a multitenant-token, the tenantId needs to be provided. 
+
+            #region Custom parameters
+
+            var tenantId = "MyTenantId"; // Only necessary for multi-tenant token.
+
+            #endregion
+
+            _output.WriteTittle("Executing Streaming.SDK example: Set subscriber file to 'deleted'");
+
+            // Configure the file id.
+            var fileId = "FileId";
+
+            // Delete the file.
+            var deleteResult = await Streaming.DeleteFileAsync(fileId: fileId, tenantId: tenantId);
+
+            Assert.True(deleteResult);
+
+            // Print the result.
+            _output.WriteLine($"File could be deleted:{deleteResult}");
+        }
+    }
+}
